f4eee2a29770ad6bc0e5176a5f7d87dd79260c63
SirMorthwyl <rodrigosantosmorgado@gmail.com>
2021-09-24 00:34:21 +0100

    Initial commit

---
+++ src/main/java/com/wylliemorth/streameranalytics/StreamerAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.wylliemorth.streameranalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamerAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamerAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wylliemorth/streameranalytics/StreamerAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wylliemorth.streameranalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamerAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
